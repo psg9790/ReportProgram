32cc7fdf8c0088e78ca181e0379197c494a8d074
psg9790 <psg9790@gmail.com>
2023-12-10 23:04:44 +0900

    dia

--- src/report/Main.java
+++ src/report/Main.java
@@ -1,10 +1,10 @@
-package Main;
+package report;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("hello, world!");
 	}
 
 }
