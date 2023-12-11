ee4c5a86ac94abc477eac22483c49fb4c280a37b
psg9790 <81291990+psg9790@users.noreply.github.com>
2023-12-11 19:45:01 +0900

    what

---
+++ src/report/PageBuilder.java
@@ -0,0 +1,18 @@
+package report;
+
+public class PageBuilder {
+	private Page frontReader;
+	private Page contentReader;
+	private Page backReader;
+	private PageOption options; 
+	
+	public void Print()
+	{
+		
+	}
+	
+	public int CalcPrice()
+	{
+		return 0;
+	}
+}
